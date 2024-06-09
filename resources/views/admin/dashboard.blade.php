4e783b9d495ac6d73be79e3ea9bdd3f8a1e6e68a
Manoj Gonte <manoj@ycstech.in>
2024-06-09 13:24:24 +0530

    fix: test case fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -56,7 +56,7 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-danger">
                         <div class="inner">
-                            <h3>{{App\Models\Invoices::count()}}</h3>
+                            <h3>{{App\Models\invoices::count()}}</h3>
                             <p>Invoices</p>
                         </div>
                         <div class="icon">
