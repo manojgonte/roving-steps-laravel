4d95dcb7c6bab7681033004dc3c10a4c6a806982
Manoj G <gontemanoj@gmail.com>
2025-01-30 23:33:36 +0530

    test push

--- resources/views/admin/tour/tour-enquiries.blade.php
+++ resources/views/admin/tour/tour-enquiries.blade.php
@@ -10,8 +10,8 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('/admin/dashboard') }}">Home</a></li>
-                        <li class="breadcrumb-item active">Tour enquiry list </li>
+                        <!-- <li class="breadcrumb-item"><a href="{{ url('/admin/dashboard') }}">Home</a></li> -->
+                        <!-- <li class="breadcrumb-item active">Tour enquiry list </li> -->
                     </ol>
                 </div>
             </div>
